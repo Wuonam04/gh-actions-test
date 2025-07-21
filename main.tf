e74e12946eeefcc3c217a8e79d267873b52fefc9
Wuonam <odeedward04@gmail.com>
2025-07-21 12:25:53 +0200

    Azure deployment

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "myapp-demo-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -24,7 +24,7 @@ resource "azurerm_app_service" "app" {
   name                = "myapp-demo"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  app_service_plan_id = azurerm_service_plan.plan.id
 
   site_config {
     linux_fx_version = "DOCKER|wuonam/gh-test:latest"
